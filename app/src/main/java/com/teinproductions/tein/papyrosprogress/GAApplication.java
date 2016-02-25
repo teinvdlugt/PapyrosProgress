fec06af9a213e0aafeffdf8ac4d9c0d1b69f7d22
Tein van der Lugt <teinvdlugt@gmail.com>
2016-02-25 21:00:07 +0100

    Update license (GNU)

--- app/src/main/java/com/teinproductions/tein/papyrosprogress/GAApplication.java
+++ app/src/main/java/com/teinproductions/tein/papyrosprogress/GAApplication.java
@@ -1,3 +1,20 @@
+/*
+ * Papyros Progress: An Android application showing the development progress of Papyros
+ * Copyright (C) 2016  Tein van der Lugt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.teinproductions.tein.papyrosprogress;
 
 import android.app.Application;
@@ -5,12 +22,6 @@ import android.app.Application;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 
-
-/**
- * Subclass of {@code android.app.Application}, needed for Google Analytics tracking.
- * Note {@code AndroidManifest.xml}, where the {@code android:name} attribute of the
- * {@code <application>} tag has been set to {@code .GAApplication}.
- */
 public class GAApplication extends Application {
 
     private Tracker mTracker;
--- app/src/main/java/com/teinproductions/tein/papyrosprogress/PapyrosRecyclerAdapter.java
+++ app/src/main/java/com/teinproductions/tein/papyrosprogress/PapyrosRecyclerAdapter.java
@@ -1,3 +1,20 @@
+/*
+ * Papyros Progress: An Android application showing the development progress of Papyros
+ * Copyright (C) 2016  Tein van der Lugt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.teinproductions.tein.papyrosprogress;
 
 import android.app.Activity;
@@ -18,7 +35,6 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
-
 
 class PapyrosRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static final int DONT_SHOW_TEXT_SIZE_TILE = -1;
